4214e24123f0550e56384d4a7a912e8768172885
Blur-mirror <antonioc.adolfo@gmail.com>
2026-03-09 11:58:18 +0000

    Fixed app.py limiter typo

--- backend/app.py
+++ backend/app.py
@@ -81,7 +81,7 @@ def home():
 
 limiter = Limiter(
     app=app,
-    key_func-get_remote_address,
+    key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"],
     storage_uri="memory://"
 )
