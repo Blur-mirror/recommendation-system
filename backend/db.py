d1dc32335313af1067ca83e9d5613bc7d6f2f8db
Blur-mirror <antonioc.adolfo@gmail.com>
2026-03-31 15:16:58 +0100

    Added render support, fixed db password Added render.yaml for deployment on Render, updated docker-compose.yml for compatibility, and fixed the database password in db.py. Also removed the vehicles.pb file which is no longer needed.

--- backend/db.py
+++ backend/db.py
@@ -7,18 +7,17 @@ import re # For input validation
 # Load .env variables
 load_dotenv()
 
-# Create DB connection using the psycogp2 library
+# Create DB connection using the psycogp2 library - modified to support render
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        return psycopg2.connect(database_url)
     return psycopg2.connect(
-
-        #It pulls 'DB_HOST' from our Docker Compose file environment
         host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-         #We use os.getenv to keep our real passwords out of the code
         password=os.getenv("DB_PASS")
     )
-
 def validate_username(username):
     """Alphanumeric + underscore, 3-20 chars"""
     if not re.match(r'^[a-zA-Z0-9_]{3,20}$', username):
